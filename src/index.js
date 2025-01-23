bdab4926b7d93c68525e83eebae068fa178c2adb
Jonas <jonasbal1999@gmail.com>
2025-01-23 18:16:03 +0200

    New pages and showroom map

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import "./custom-styles.css";
 import Lenis from "lenis";
 import { gsap } from "gsap";
+import { Draggable } from "gsap/Draggable";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 // core version + navigation, pagination modules:
 import Swiper from "swiper";
@@ -13,6 +14,7 @@ import "swiper/css/effect-fade";
 // Overlay scrollbar
 import "overlayscrollbars/overlayscrollbars.css";
 import { OverlayScrollbars, ScrollbarsHidingPlugin, SizeObserverPlugin, ClickScrollPlugin } from "overlayscrollbars";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
 
 // Global lenis instance
 let lenis;
@@ -40,6 +42,12 @@ function nestedLenisScroll() {
 
 	nestedTargets.forEach((wrapper) => {
 		const content = wrapper.querySelector('[data-nested-lenis="content"]');
+		const isMobile = window.matchMedia("(max-width: 768px)").matches; // Adjust breakpoint as needed
+
+		// Skip Lenis initialization on showroom mobile
+		if (isMobile && wrapper.getAttribute("data-disable-lenis") === "mobile") {
+			return;
+		}
 
 		const nestedLenis = new Lenis({
 			wrapper: wrapper,
@@ -1723,9 +1731,559 @@ function customFormSelect() {
 	});
 }
 
+function faqAnchorScroll() {
+	const anchorLinks = document.querySelectorAll("[data-faq-anchor]");
+	if (!anchorLinks.length) return;
+
+	const anchorSections = Array.from(anchorLinks).map((anchor) => {
+		const sectionId = "faq-" + anchor.getAttribute("data-faq-anchor");
+		return document.getElementById(sectionId);
+	});
+
+	// Loop through each section and set up a ScrollTrigger
+	anchorSections.forEach((section, index) => {
+		if (!section) return;
+
+		ScrollTrigger.create({
+			trigger: section,
+			start: "top 30%",
+			end: "bottom 30%",
+			toggleClass: {
+				targets: anchorLinks[index], // Add class to the corresponding link
+				className: "is-current",
+			},
+		});
+	});
+
+	anchorLinks.forEach((link) => {
+		link.addEventListener("click", (e) => {
+			// Get target faq section
+			const targetId = link.getAttribute("data-faq-anchor");
+			const targetElement = document.getElementById("faq-" + targetId);
+
+			if (targetElement) {
+				lenis.scrollTo(targetElement, {
+					duration: 1,
+					offset: -window.innerHeight * 0.2, // Offset by 40% of viewport height
+					easing: (x) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2), // Ease-in-out easing
+				});
+			}
+		});
+	});
+}
+
+function draggableCardsAnimation() {
+	const cards = document.querySelectorAll('[data-draggable-cards="card"]');
+
+	if (!cards.length) return;
+
+	const cardsContainer = document.querySelector('[data-draggable-cards="container"]');
+
+	let mm = gsap.matchMedia();
+
+	mm.add("(min-width: 769px)", () => {
+		cards.forEach((card) => {
+			Draggable.create(card, {
+				bounds: cardsContainer,
+				onClick: function () {
+					console.log("clicked");
+				},
+				onDragEnd: function () {
+					console.log("drag ended");
+				},
+			});
+		});
+	});
+}
+
+function aboutValuesListHover() {
+	const valueItems = document.querySelectorAll('[data-values-hover="item"]');
+
+	if (!valueItems.length) return;
+
+	// Set active item
+	function setActiveItem(hoveredItem) {
+		valueItems.forEach((item) => {
+			item.classList.toggle("is-active", item === hoveredItem);
+		});
+	}
+
+	// Disable hover on all except the current item
+	function debounceHover(hoveredItem) {
+		valueItems.forEach((item) => {
+			item.classList.toggle("no-hover", item !== hoveredItem);
+		});
+	}
+
+	// Remove hover locks
+	function resetHoverState() {
+		valueItems.forEach((item) => {
+			item.classList.remove("no-hover");
+		});
+	}
+
+	// Event Listeners
+	valueItems.forEach((item, index) => {
+		if (index === 0) {
+			setActiveItem(item);
+		}
+
+		item.addEventListener("mouseenter", () => {
+			debounceHover(item);
+			setActiveItem(item);
+		});
+
+		item.addEventListener("mouseleave", resetHoverState);
+	});
+}
+
+function aboutGallerySlider() {
+	const swiperTarget = document.querySelector(".about-gallery_swiper");
+
+	if (!swiperTarget) return;
+
+	const swiper = new Swiper(swiperTarget, {
+		modules: [Autoplay],
+		loop: true,
+		speed: 800,
+		autoplay: {
+			delay: 1000,
+		},
+
+		centeredSlides: true,
+		spaceBetween: 20,
+		slidesPerView: 1.8,
+
+		breakpoints: {
+			992: {
+				slidesPerView: 5,
+				spaceBetween: 32,
+			},
+			768: {
+				slidesPerView: 4,
+			},
+		},
+	});
+}
+
+function aboutValuesSlider() {
+	const swiperTarget = document.querySelector(".about-values_swiper");
+
+	if (!swiperTarget) return;
+
+	const swiper = new Swiper(swiperTarget, {
+		modules: [Navigation],
+		slidesPerView: 1.3,
+		speed: 600,
+		spaceBetween: 16,
+		breakpoints: {
+			992: {
+				slidesPerView: 2.7,
+				spaceBetween: 20,
+			},
+			768: {
+				slidesPerView: 2.7,
+			},
+		},
+		// Navigation arrows
+		navigation: {
+			disabledClass: "is-disabled",
+			nextEl: ".about-values_slider_nav-btn.is-next",
+			prevEl: ".about-values_slider_nav-btn.is-prev",
+		},
+	});
+}
+
+function aboutCardsExpand() {
+	const button = document.querySelector('[data-about-expand="button"]');
+	if (!button) return;
+
+	const container = document.querySelector('[data-about-expand="container"]');
+	const cardsWrap = container.querySelector('[data-about-expand="cards-wrap"]');
+	const overlay = container.querySelector('[data-about-expand="overlay"]');
+
+	const cardsHeight = cardsWrap.offsetHeight;
+
+	button.addEventListener("click", () => {
+		gsap.to(container, {
+			height: cardsHeight,
+			duration: 0.5,
+			ease: "power2.inOut",
+		});
+		gsap.set(overlay, {
+			autoAlpha: 0,
+		});
+	});
+}
+
+async function showroomMap() {
+	const mapContainer = document.querySelector('[data-showroom-map="container"]');
+
+	if (!mapContainer) return;
+
+	const showroomListItems = document.querySelectorAll("[data-showroom-list-item]");
+	let markers = [];
+
+	// Importing libraries
+	const { Map } = await google.maps.importLibrary("maps");
+	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+	const zoomInButton = document.querySelector('[data-showroom-zoom-btn="zoom-in"]');
+	const zoomOutButton = document.querySelector('[data-showroom-zoom-btn="zoom-out"]');
+
+	// Showroom content wraps
+	const listWrap = document.querySelector('[data-showroom-list="wrap"]');
+	const infoWrap = document.querySelector('[data-showroom-info="wrap"]');
+
+	// Active marker var for removing class when another info window is open
+	let activeMarker = null;
+
+	// Showroom info data elements
+	const showroomClose = document.querySelector('[data-showroom-info="close"]');
+	const showroomInfoName = document.querySelector('[data-showroom-info="name"]');
+	const showroomInfoDescription = document.querySelector('[data-showroom-info="description"]');
+	const showroomInfoAddress = document.querySelector('[data-showroom-info="address"]');
+	const showroomInfoHours = document.querySelector('[data-showroom-info="hours"]');
+	const showroomInfoBooking = document.querySelector('[data-showroom-info="booking"]');
+
+	// Functions
+	function displayMarkerTooltip(name, address, marker) {
+		infoWindow.setContent(`<div class="showroom_map_tooltip-wrap">
+			<h3 class="button-style-s">${name}</h3>
+			<span class="button-style-s text-color-dark-grey">${address}</span>
+			</div>`);
+		infoWindow.open(marker.map, marker);
+		map.setZoom(6);
+	}
+
+	function hideMarkerTooltip() {
+		infoWindow.close();
+		map.setZoom(5);
+	}
+
+	function displayFullInfo(itemData, marker) {
+		showroomInfoName.textContent = itemData.name;
+		showroomInfoDescription.textContent = itemData.description;
+		showroomInfoAddress.textContent = itemData.address;
+		showroomInfoBooking.href = itemData.booking;
+		// Spliting working hours by ;
+		const splitHours = itemData.hours.split(";");
+		showroomInfoHours.innerHTML = splitHours.join("<br>");
+
+		if (activeMarker) {
+			activeMarker.content.classList.remove("is-open");
+		}
+
+		marker.content.classList.add("is-open");
+		activeMarker = marker;
+
+		let tl = gsap.timeline();
+
+		tl.set(listWrap, {
+			position: "absolute",
+		})
+			.set(infoWrap, {
+				position: "static",
+			})
+			.to(infoWrap, {
+				autoAlpha: 1,
+				duration: 0.2,
+				ease: "linear",
+			})
+			.to(
+				listWrap,
+				{
+					autoAlpha: 0,
+					duration: 0.2,
+					ease: "linear",
+				},
+				"<"
+			);
+	}
+
+	function hideFullInfo() {
+		if (activeMarker) {
+			activeMarker.content.classList.remove("is-open");
+		}
+
+		let tl = gsap.timeline();
+
+		tl.set(infoWrap, {
+			position: "absolute",
+		})
+			.set(listWrap, {
+				position: "static",
+			})
+			.to(listWrap, {
+				autoAlpha: 1,
+				duration: 0.2,
+				ease: "linear",
+			})
+			.to(
+				infoWrap,
+				{
+					autoAlpha: 0,
+					duration: 0.2,
+					ease: "linear",
+				},
+				"<"
+			);
+	}
+
+	// 1. Initialize the map
+	const map = new Map(mapContainer, {
+		center: { lat: 51.0569311, lng: 5.177956 },
+		zoom: 5,
+		fullscreenControl: false,
+		mapTypeControl: false,
+		streetViewControl: false,
+		zoomControl: false,
+		mapId: "YOUR_MAP_ID_HERE", // Replace with your valid Map ID
+		gestureHandling: "cooperative",
+	});
+
+	// Create an InfoWindow instance
+	const infoWindow = new google.maps.InfoWindow();
+
+	// 2. Add markers to the map
+	showroomListItems.forEach((listItem) => {
+		// Getting data for each list item
+		let itemData = {};
+		itemData.name = listItem.getAttribute("data-showroom-list-name");
+		itemData.address = listItem.getAttribute("data-showroom-list-address");
+		itemData.description = listItem.getAttribute("data-showroom-list-description");
+		itemData.slug = listItem.getAttribute("data-showroom-list-item");
+		itemData.lat = Number(listItem.getAttribute("data-showroom-list-lat"));
+		itemData.lng = Number(listItem.getAttribute("data-showroom-list-lng"));
+		itemData.hours = listItem.getAttribute("data-showroom-list-hours");
+		itemData.booking = listItem.getAttribute("data-showroom-list-booking");
+
+		// Creating custom marker
+		const customMarker = document.createElement("div");
+		customMarker.className = "showroom_map_marker-wrap";
+		customMarker.setAttribute("data-showroom-marker-item", itemData.slug);
+		customMarker.innerHTML = `
+		<div class="showroom_map_marker-icon">
+            <svg width="100%" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M8.00078 1.3999C10.5968 1.3999 12.7008 3.5039 12.7008 6.0999C12.7008 10.9649 8.00078 14.6249 8.00078 14.6249C8.00078 14.6249 3.30078 10.9649 3.30078 6.0999C3.30078 3.5039 5.40478 1.3999 8.00078 1.3999Z" stroke="currentColor" stroke-miterlimit="10"/>
+	<path d="M8.00116 7.79284C8.87984 7.79284 9.59216 7.08052 9.59216 6.20184C9.59216 5.32315 8.87984 4.61084 8.00116 4.61084C7.12247 4.61084 6.41016 5.32315 6.41016 6.20184C6.41016 7.08052 7.12247 7.79284 8.00116 7.79284Z" stroke="currentColor" stroke-miterlimit="10"/>
+	</svg>
+	</div>
+	
+        `;
+
+		// Add an advanced marker
+		const marker = new AdvancedMarkerElement({
+			map,
+			position: { lat: itemData.lat, lng: itemData.lng },
+			content: customMarker,
+			gmpClickable: true,
+			title: itemData.name,
+		});
+
+		markers.push(marker);
+
+		marker.addListener("click", () => {
+			displayMarkerTooltip(itemData.name, itemData.address, marker);
+			displayFullInfo(itemData, marker);
+		});
+
+		marker.content.addEventListener("mouseenter", () => {
+			const relatedListItem = document.querySelector(`[data-showroom-list-item=${itemData.slug}]`);
+			relatedListItem.classList.add("is-hovered");
+		});
+
+		marker.content.addEventListener("mouseleave", () => {
+			const relatedListItem = document.querySelector(`[data-showroom-list-item=${itemData.slug}]`);
+			relatedListItem.classList.remove("is-hovered");
+		});
+
+		listItem.addEventListener("mouseenter", () => {
+			const relatedMarker = document.querySelector(`[data-showroom-marker-item="${itemData.slug}"]`);
+			relatedMarker.classList.add("is-hovered");
+		});
+
+		listItem.addEventListener("click", () => {
+			displayMarkerTooltip(itemData.name, itemData.address, marker);
+			displayFullInfo(itemData, marker);
+		});
+
+		listItem.addEventListener("mouseleave", () => {
+			const relatedMarker = document.querySelector(`[data-showroom-marker-item="${itemData.slug}"]`);
+			relatedMarker.classList.remove("is-hovered");
+		});
+	});
+
+	// Add event listeners for zoom buttons
+	zoomInButton.addEventListener("click", () => map.setZoom(map.getZoom() + 1));
+	zoomOutButton.addEventListener("click", () => map.setZoom(map.getZoom() - 1));
+
+	// Add event listener to info wrap close btn
+	showroomClose.addEventListener("click", () => {
+		hideFullInfo();
+		hideMarkerTooltip();
+	});
+
+	// 3. Add clustering with a custom renderer
+	new MarkerClusterer({
+		markers,
+		map,
+		renderer: {
+			render: ({ count, position }) => {
+				// Create custom cluster content
+				const clusterElement = document.createElement("div");
+				clusterElement.className = "showroom_map_cluster-marker caption-style-m";
+				clusterElement.textContent = count; // Number of markers in the cluster
+				return new google.maps.marker.AdvancedMarkerElement({
+					position,
+					content: clusterElement,
+					gmpClickable: true,
+				});
+			},
+		},
+	});
+}
+
+function showroomSearch() {
+	const searchInput = document.querySelector('[data-showroom-search="input"]');
+	const listItems = document.querySelectorAll('[data-showroom-search="item"]');
+
+	if (!searchInput || !listItems.length) return;
+
+	let listArray = [];
+
+	const listWrap = document.querySelector('[data-showroom-search="list"]');
+	const clearBtn = document.querySelector('[data-showroom-search="clear"]');
+	const form = document.querySelector('[data-showroom-search="form"]');
+	const emptyBlock = document.querySelector('[data-showroom-search="empty-state"]');
+	const loadMoreButton = document.querySelector('[data-showroom-load-more="btn"]');
+
+	let loadMoreClicked = false;
+	let loadMorePosts = [];
+	let isEmptyState = false;
+
+	// Creating object with list elements and their filter values
+
+	listItems.forEach((post, index) => {
+		let postData = {};
+		// Get post values for filtering
+		postData.element = post;
+		postData.cityName = post.getAttribute("data-showroom-list-name").toLowerCase();
+		postData.cityAddress = post.getAttribute("data-showroom-list-address").toLowerCase();
+
+		// Create object entry
+		listArray.push(postData);
+	});
+
+	function showEmptyState() {
+		let tl = gsap.timeline();
+
+		tl.set(emptyBlock, {
+			display: "flexGe",
+		}).to(emptyBlock, {
+			autoAlpha: 1,
+			duration: 0.2,
+			ease: "linear",
+		});
+
+		isEmptyState = true;
+	}
+
+	function hideEmptyState() {
+		let tl = gsap.timeline();
+
+		tl.set(emptyBlock, {
+			display: "none",
+			autoAlpha: 0,
+		});
+
+		isEmptyState = false;
+	}
+
+	function filterItems(filterValue = "") {
+		let filteredElements = [];
+		listArray.forEach((post) => {
+			// Check if either the city name or the address matches the input
+			if (post.cityName.includes(filterValue) || post.cityAddress.includes(filterValue)) {
+				// Show valid posts
+				filteredElements.push(post.element);
+			}
+		});
+
+		return filteredElements;
+	}
+
+	function updateList(filteredElements) {
+		// Reset loadMorePosts array
+		loadMorePosts = [];
+		filteredElements.forEach((element, index) => {
+			if (index > 4 && !loadMoreClicked) {
+				element.classList.add("load-more-hide");
+				loadMorePosts.push(element);
+			}
+			listWrap.append(element);
+		});
+	}
+
+	function showLoadMorePosts() {
+		loadMorePosts.forEach((post) => {
+			post.classList.remove("load-more-hide");
+		});
+
+		loadMoreButton.style.display = "none";
+		loadMoreClicked = true;
+	}
+
+	function filterController(filterValue) {
+		// 1. Get filtered elements array
+		let filteredElements = filterItems(filterValue);
+
+		// 2. Handle emtpy state
+		if (!isEmptyState && !filteredElements.length) {
+			showEmptyState();
+		} else if (isEmptyState && filteredElements.length) {
+			hideEmptyState();
+		}
+
+		// 3. Clear DOM list
+		listWrap.innerHTML = "";
+
+		// 4. Append filtered items
+		updateList(filteredElements);
+
+		// 5. Show or hide "View all" on mobile
+		if (!loadMoreClicked && filteredElements.length > 5) {
+			loadMoreButton.style.display = "block";
+		} else if (!loadMoreClicked && filteredElements.length <= 5) {
+			loadMoreButton.style.display = "none";
+		}
+	}
+
+	filterController("");
+
+	clearBtn.addEventListener("click", () => {
+		searchInput.value = "";
+		filterController();
+	});
+
+	searchInput.addEventListener("keyup", () => {
+		const filterValue = searchInput.value.toLowerCase();
+		filterController(filterValue);
+	});
+
+	loadMoreButton.addEventListener("click", showLoadMorePosts);
+
+	// Prevent search input form from submitting
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		event.stopPropagation();
+	});
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 	overlayScrollbar();
 	gsap.registerPlugin(ScrollTrigger);
+	gsap.registerPlugin(Draggable);
 	smoothScroll();
 	nestedLenisScroll();
 	$.validator.setDefaults({
@@ -1764,6 +2322,14 @@ document.addEventListener("DOMContentLoaded", () => {
 	formPageField();
 	footerParallax();
 	customFormSelect();
+	faqAnchorScroll();
+	draggableCardsAnimation();
+	aboutValuesListHover();
+	aboutGallerySlider();
+	aboutValuesSlider();
+	aboutCardsExpand();
+	showroomMap();
+	showroomSearch();
 	setTimeout(() => {
 		ScrollTrigger.sort();
 		ScrollTrigger.refresh();
